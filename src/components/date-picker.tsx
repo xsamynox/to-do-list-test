d4647e86e4795a3455063b44db787b37c6bbae92
Samantha Moreno <xsamynox@gmail.com>
2023-08-22 15:27:31 -0400

    feat(datePicker): a datapicker was created to handle card dates

---
+++ src/components/date-picker.tsx
@@ -0,0 +1,19 @@
+type CalendarProps = {
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+};
+
+const Calendar = ({ onChange, value }: CalendarProps) => {
+  return (
+    <form>
+      <input
+        type="date"
+        value={value}
+        onChange={onChange}
+        className="w-auto bg-neutral-50 border border-neutral-200 text-gray-900 text-sm rounded-lg block p-2.5 shadow-sm hover:shadow-md shadow-neutral-200"
+      />
+    </form>
+  );
+};
+
+export default Calendar;
